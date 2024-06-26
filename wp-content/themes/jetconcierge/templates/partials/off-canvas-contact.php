568831e09a2a8a2dd332a5c613573aed4ebd9344
sourcecodestudiouk <connor.banks@sourcecodestudio.co.uk>
2024-06-26 09:39:13 +0100

    form addition

--- wp-content/themes/jetconcierge/templates/partials/off-canvas-contact.php
+++ wp-content/themes/jetconcierge/templates/partials/off-canvas-contact.php
@@ -24,7 +24,7 @@
                 </div>
             </div>
             <div class="form-container">
-                <?= do_shortcode('[forminator_form id="66"]'); ?>
+                <?= do_shortcode('[forminator_form id="607"]'); ?>
             </div>
         </div>
     </div>
