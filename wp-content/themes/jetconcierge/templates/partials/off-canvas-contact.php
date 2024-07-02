d1f6b502763eb2c10ee6d20f033bf9eca5ae6890
sourcecodestudiouk <connor.banks@sourcecodestudio.co.uk>
2024-07-02 13:40:14 +0100

    Updated ID for contact form

--- wp-content/themes/jetconcierge/templates/partials/off-canvas-contact.php
+++ wp-content/themes/jetconcierge/templates/partials/off-canvas-contact.php
@@ -24,7 +24,7 @@
                 </div>
             </div>
             <div class="form-container">
-                <?= do_shortcode('[forminator_form id="66"]'); // Needs to be returned to 607 for LIVE ?>
+                <?= do_shortcode('[forminator_form id="607"]'); // Needs to be returned to 607 for LIVE ?>
             </div>
         </div>
     </div>
