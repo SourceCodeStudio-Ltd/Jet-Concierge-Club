43bff61af2cd3f24ab08818b4785826a510cfe25
sourcecodestudiouk <connor.banks@sourcecodestudio.co.uk>
2024-07-16 11:31:00 +0100

    font weight change in logo

--- wp-content/themes/jetconcierge/assets/img/jet-con-full-text.svg.php
+++ wp-content/themes/jetconcierge/assets/img/jet-con-full-text.svg.php
@@ -1,3 +1,3 @@
 <svg xmlns="http://www.w3.org/2000/svg" width="281" height="24" viewBox="0 0 281 24">
-    <text data-name="JET CONCIERGE CLUB" transform="translate(141 19)" style="fill:#fff;font-size:18px;font-family:futura-pt, sans-serif;font-weight:600;letter-spacing:.295em"><tspan x="-139.82" y="0">JET CONCIERGE CLUB</tspan></text>
+    <text data-name="JET CONCIERGE CLUB" transform="translate(141 19)" style="fill:#fff;font-size:18px;font-family:futura-pt, sans-serif;font-weight:500;letter-spacing:.295em"><tspan x="-139.82" y="0">JET CONCIERGE CLUB</tspan></text>
 </svg>
